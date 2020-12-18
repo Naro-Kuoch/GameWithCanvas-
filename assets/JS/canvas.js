23e5b346c68f2b2efbd3e2dd386ea42948ce80ab
Naro Kuoch <narokuoch@gmail.com>
2020-12-18 15:55:44 +0700

    Version 2 display score and time

--- assets/JS/canvas.js
+++ assets/JS/canvas.js
@@ -1,19 +1,22 @@
 var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
-
+var context = canvas.getContext('2d');
+canvas.width=800;
+canvas.height=600;
 //score and time 
+
+
 var score = 0, time =60;
 
 //propertiy of the big square 
 var x = 50;
 var y=50;
-var speed =3;
-var sideLength = 20;
+var speed =6;
+var sideLength = 70;
 
 //target property
 var targetX=270;
 var targetY= 50;
-var targetLength=10;
+var targetLength=30;
 
 var up=false;
 var down=false;
@@ -21,21 +24,56 @@ var left=false;
 var right=false;
 
 
+
+
 function clear(){
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(0,0,1000,600);
+    context.fillStyle = "#EAE0DE";
+    context.fillRect(0,0,1000,600);
     
 }
 function isWithin(a, b, c) {
     return (a>b && a<c);
 }
 function respawn() {
-    targetX = Math.round(Math.random() * canvas.width - targetLength);
-    targetY = Math.round(Math.random() * canvas.height - targetLength);
+    targetX = Math.round(Math.random() * (canvas.width - targetLength));
+    targetY = Math.round(Math.random() * (canvas.height - targetLength));
   }
+
+function drawScoreTime(score,x,y,size,area,color){
+    return{
+        x:x,
+        y:y,
+        s:size,
+        a:area,
+        c:color,
+        score:score,
+        draw:function(){
+            context.beginPath();
+            context.font=this.s+'px sans-serif';
+            context.fillStyle=this.c;
+            context.textAlign=this.a;
+            context.fillText(score, this.x, this.y);
+            context.closePath();
+        }
+    }
+}
+    // context.fillStyle="black";
+    // context.font="18px Verdana";
+    // context.context.textAlign='center'; 
+    // context.fillText("Score: ",+score, canvas.width/2, canvas.height/2); 
+
 function draw(){
     //clear screen
-    clear();
+      clear();
+    // context.fillStyle='#EAE0DE';
+    // context.fillRect(0,0,canvas.width,canvas.height);
+    // context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    //display score and time
+    let sc = drawScoreTime("Score: " + score,730,40,30,"right","black");
+    sc.draw();
+    let t = drawScoreTime("Time: " + time,20,40,30,"left",'black');
+    t.draw();
     
     //update location base on keyboard
     if(down){
@@ -69,28 +107,35 @@ function draw(){
     if(isWithin(targetY, y, y + sideLength) || isWithin(targetY + targetLength, y , y+sideLength)) {
       // Respawn target: delete old target, and random new target at other location
       respawn();
+    //   sideLength+=10;
       // Increase score
       score++;
-      console.log("get score", score);
+      console.log("score", score);
+    }
+    if(sideLength==canvas.width){
+        clear();
+        
     }
   }
     
     //draw
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(x, y, sideLength, sideLength);
+    context.fillStyle = "#FF0000";
+    context.fillRect(x, y, sideLength, sideLength);
 
 
 
 
 
     // draw target
-    ctx.fillStyle ="#0BC420 "
-    ctx.fillRect(targetX, targetY, targetLength, targetLength);
+    context.fillStyle ="#0BC420 "
+    context.fillRect(targetX, targetY, targetLength, targetLength);
 
  
 
     
     if(time<=0){
+        alert("Your Score "+ score)
+        clearInterval(st);
         //end game
     }
     else{
@@ -98,10 +143,12 @@ function draw(){
     }
     
 }
-setInterval(function(){
+var st = setInterval(function(){
     time--;
     console.log('time', time);
 }, 1000);
+
+
 
 canvas.addEventListener('keydown',function(event){
     event.preventDefault();
@@ -124,6 +171,8 @@ canvas.addEventListener('keydown',function(event){
     else if (event.key == "ArrowLeft"){
         left=true;
     }
+
+    
 });
 // canvas.addEventListener('key',function(event){
 //     event.preventDefault();
@@ -132,5 +181,6 @@ canvas.addEventListener('keydown',function(event){
 
 
 draw();
+
 
 
